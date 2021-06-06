1afedc37a322af66cee4409745cbd2c62ad8463a
Mathew Chan <mchan6466@gmail.com>
2021-06-06 17:03:02 +0800

    use sudachi

---
+++ deckparser.py
@@ -0,0 +1,60 @@
+from dictionary import EXAMPLE_LIMIT
+import json
+import os
+from pathlib import Path
+from sudachipy import tokenizer
+from sudachipy import dictionary
+
+tokenizer_obj = dictionary.Dictionary().create()
+mode = tokenizer.Tokenizer.SplitMode.A
+
+bundle_path = os.path.dirname(os.path.abspath(__file__))
+EXAMPLE_PATH = Path(bundle_path, 'resources', 'anime')
+
+def getDeckStructure(folder_name):
+    file = Path(EXAMPLE_PATH, folder_name, 'deck-structure.json')
+    with open(file, encoding='utf-8') as f:
+        data = json.load(f)
+        return data
+
+def parseDeck(folder_name):
+    deck_structure = getDeckStructure(folder_name)
+    file = Path(EXAMPLE_PATH, folder_name, 'deck.json')
+    examples = []
+    with open(file, encoding='utf-8') as f:
+        data = json.load(f)
+        notes = data['notes']
+        deck_name = data['name']
+        for note in notes:
+
+            # segmentation
+            text = note['fields'][deck_structure['text-column']]
+            word_base_list = [m.normalized_form() for m in tokenizer_obj.tokenize(text, mode)]
+            word_list = [m.surface() for m in tokenizer_obj.tokenize(text, mode)]
+
+            example = {
+                'id': note['fields'][deck_structure['id-column']],
+                'deck_name': deck_name,
+                'sentence': text,
+                'word_base_list': word_base_list,
+                'word_list': word_list,
+                'translation': note['fields'][deck_structure['translation-column']],
+                'image': note['fields'][deck_structure['image-column']].split('src="')[1].split('">')[0],
+                'sound': note['fields'][deck_structure['sound-column']].split('sound:')[1].split(']')[0]
+            }
+            examples.append(example)
+
+    with open(Path(EXAMPLE_PATH, folder_name, 'data.json'), 'w', encoding='utf8') as outfile:
+        json.dump(examples, outfile, indent=4, ensure_ascii=False)
+
+parseDeck('Anime - Your Name')
+
+# def get_base_form(word):
+#     return tokenizer_obj.tokenize(word, mode)[0].normalized_form()
+#     word_base_list = [m.surface() for m in tokenizer_obj.tokenize(word, mode)]
+#     return word_base_list[0]
+
+# a = get_base_form("食べて")
+# print(a)
+
+
--- dictionary.py
+++ dictionary.py
@@ -2,10 +2,12 @@ from json import decoder
 import json
 import os
 import zipfile
-from fugashi import Tagger
 from pathlib import Path
 from bottle import abort, request
 from socket import gethostname, gethostbyname 
+
+from sudachipy import tokenizer
+from sudachipy import dictionary
 
 bundle_path = os.path.dirname(os.path.abspath(__file__))
 DICTIONARY_PATH = Path(bundle_path, 'resources', 'dictionaries')
@@ -15,17 +17,25 @@ EXAMPLE_LIMIT = 3
 
 dictionary_map = {}
 example_map = {}
+tokenizer_obj = dictionary.Dictionary().create()
+mode = tokenizer.Tokenizer.SplitMode.A
+
+def get_base_form(word):
+    word_base_list = [m.normalized_form() for m in tokenizer_obj.tokenize(word, mode)]
+    return word_base_list[0]
 
 def parse(text):
-    tagger = Tagger('-Owakati')
-    tagger.parse(text)
-    word_base_list = [word.feature.lemma for word in tagger(text)]
+    word_base_list = [m.normalized_form() for m in tokenizer_obj.tokenize(text, mode)]
     return word_base_list
 
 def look_up(text):
-    text = text.replace(" ", "")
-    word_bases = parse(text)
-    words = [word for word in word_bases if word in dictionary_map]
+    text = text.replace(" ", "") 
+    words = []
+    if (text in dictionary_map):
+        words = [get_base_form(text)]
+    else:
+        word_bases = parse(text)
+        words = [word for word in word_bases if word in dictionary_map]
     result = [
         {
             'dictionary': 
@@ -70,53 +80,38 @@ def load_dictionary(dictionary_name):
         print('failed to find path for dictionary')
 
 def load_example_by_path(example_path):
-    notes = []
-    deck_name = ''
-    file = Path(example_path, 'deck.json')
+    examples = []
+    file = Path(example_path, 'data.json')
     with open(file, encoding='utf-8') as f:
-        data = json.load(f)
-        notes = data['notes']
-        deck_name = data['name']
+        examples = json.load(f)
     
     output_map = {}
-    print('notes', len(notes))
-    for note in notes:
-        note = parse_note(note, deck_name)
-        sentence = note['fields'][1]
+    for example in examples:
+        example = parse_example(example)
+        sentence = example['sentence']
         if sentence is not None:
-            words = note['word_base_list']
+            words = example['word_base_list']
             for (index, word) in enumerate(words):
                 if (word not in dictionary_map) or word in '？?!.,':
                     continue
-                custom_note = note
-                custom_note['word_index'] = index
+                custom_example = example
+                custom_example['word_index'] = index
                 if word in output_map:
                     if (len(output_map[word]) < EXAMPLE_LIMIT):
-                        output_map[word].append(dict(custom_note))
+                        output_map[word].append(dict(custom_example))
                 else:
-                    output_map[word] = [dict(custom_note)] 
+                    output_map[word] = [dict(custom_example)] 
     return output_map
 
-def parse_note(note, deck_name):
-    # tagging
-    text = note['fields'][1]
-    tagger = Tagger('-Owakati')
-    tagger.parse(text)
-    note['word_base_list'] = [word.feature.lemma for word in tagger(text)]
-    note['word_list'] = [str(word) for word in tagger(text)]
-
+def parse_example(example):
     # image
-    image_value = note['fields'][7]
-    image_name = image_value.split('src="')[1].split('">')[0]
-    image_path = '{}/anime/{}/media/{}'.format(HOST, deck_name, image_name)
-    note['image_url'] = image_path
+    image_path = '{}/anime/{}/media/{}'.format(HOST, example['deck_name'], example['image'])
+    example['image_url'] = image_path
     
     # sound
-    sound_value = note['fields'][8]
-    sound_name = sound_value.split('sound:')[1].split(']')[0]
-    sound_path = '{}/anime/{}/media/{}'.format(HOST, deck_name, sound_name)
-    note['sound_url'] = sound_path
-    return note
+    sound_path = '{}/anime/{}/media/{}'.format(HOST, example['deck_name'], example['sound'])
+    example['sound_url'] = sound_path
+    return example
 
 def load_examples(media_name):
     global example_map
@@ -128,3 +123,4 @@ def load_examples(media_name):
 
 load_dictionary('jmdict_english')
 load_examples('Anime - Your Name')
+# print(look_up('ゆめ'))
