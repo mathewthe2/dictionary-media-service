ddef0a5216ebf5b035e3c5b45f48d35661f6f55f
Mathew Chan <mchan6466@gmail.com>
2021-06-10 14:57:41 +0800

    remove unused function

--- japanesetokenizer.py
+++ japanesetokenizer.py
@@ -4,10 +4,6 @@ from sudachipy import dictionary
 tokenizer_obj = dictionary.Dictionary().create()
 mode = tokenizer.Tokenizer.SplitMode.A
 
-def get_base_form(word):
-    word_base_list = [m.normalized_form() for m in tokenizer_obj.tokenize(word, mode)]
-    return word_base_list[0]
-
 def analyze_japanese(text):
     tokens = [m for m in tokenizer_obj.tokenize(text, mode)]
     return {
