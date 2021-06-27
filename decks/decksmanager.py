4a8a7b4ff30fb469c7870a9a6e5ccc4ec4b09120
Mathew Chan <mchan6466@gmail.com>
2021-06-27 18:10:17 +0800

    remove comments and use default category in config

--- decks/decksmanager.py
+++ decks/decksmanager.py
@@ -1,17 +1,16 @@
 
 from decks.decks import Decks 
-from config import DECK_CATEGORIES
+from config import DECK_CATEGORIES, DEFAULT_CATEGORY
 
 class DecksManager:
-    def __init__(self):
+    def __init__(self, category=DEFAULT_CATEGORY):
         self.decks = {}
-        self.category = 'anime'
+        self.category = category
 
     def set_category(self, category):
         self.category = category
 
     def load_decks(self):
-        # TODO: refactor to combine anime and literature deck into one class
         for deck_category in DECK_CATEGORIES:
             self.decks[deck_category] = Decks(
                 category=deck_category, 
@@ -20,13 +19,6 @@ class DecksManager:
                 has_sound=DECK_CATEGORIES[deck_category]["has_sound"])
             self.decks[deck_category].load_decks()
         
-        # anime_decks = AnimeDecks()
-        # anime_decks.load_decks()
-        # self.decks['anime'] = anime_decks
-        # literature_decks = LiteratureDecks()
-        # literature_decks.load_decks()
-        # self.decks['literature'] = literature_decks
-
     def get_sentence(self, sentence_id):
         return self.decks[self.category].get_sentence(sentence_id)
 
